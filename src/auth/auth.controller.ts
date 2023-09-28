d45cd2d7136a98cb22e3a27965bff812904a7caf
telman03 <telmangadimov1@gmail.com>
2023-09-28 12:51:11 +0400

    Initial commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @Post('/local/signup')
+    signupLocal(@Body() dto: AuthDto) {
+        this.authService.signupLocal(dto);
+    }
+
+    @Post('/local/signin')
+    signinLocal() {
+        this.authService.signinLocal(); 
+    }
+
+    @Post('/logout')
+    logout() {
+        this.authService.logout();
+    }
+
+    @Post('/refresh')
+    refreshTokens() { 
+        this.authService.refreshTokens();
+    }
+}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient 
+        implements OnModuleInit, OnModuleDestroy
+    {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: 'postgresql://postgres:postgres@localhost:5432/nestjs?schema=public',
+                },
+            },
+
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
