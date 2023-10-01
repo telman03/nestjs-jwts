6b313fc16f4379ece88a584db2dfbea8f488f46c
telman03 <telmangadimov1@gmail.com>
2023-10-01 21:13:35 +0400

    OpenApi swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AtGuard } from './common/guards';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  // const reflector = new Reflector();
-  // app.useGlobalGuards(new AtGuard(reflector));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Auth')
+    .setDescription('The NestJS Auth API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
